fd944dadb3afe691685ef24977801a9a74fc1215
Guilherme Luvizute <luvizute@gmail.com>
2020-07-25 15:11:37 -0300

    Variáveis

--- variaveis.py
+++ variaveis.py
@@ -1,4 +1,30 @@
 # Variável: serve para armazenar um valor na memória, atribui um nome a esse valor para ser usado posteriormente
 minha_idade = 19
 print("Eu tenho",minha_idade,"anos")
-# 
+
+# Eu não posso começar uma variável com número, SyntaxError
+# Eu não posso usar espaço
+# Não posso usar letras maiúsculas, o Python não identifica
+# Nomes autoexplicativos são mais fáceis de reconhecer
+
+taxaUSD = 3.20
+valor = 300 / taxaUSD
+print(valor)
+
+# Variáveis muito longas podem ser ruins para reescrever depois, tentar criar variáveis curtas mas autoexplicativs
+# Não usar varíaveis com o mesmo nome das keywords do python (https://www.w3schools.com/python/python_ref_keywords.asp)
+# Usar o mesmo nome de varíavel, uma com letra maiúscula e outra com minúscula, com valores diferentes pode causar confusão
+# No python não é necessário antes de inicar o programa declarar nenhuma variável nem dizer o tipo
+
+meuNome = "Dóris"
+print(meuNome)
+
+# Comando 'type' serve para te mostrar qual o tipo da váriavel
+print(type(meuNome))
+print(type(taxaUSD))
+print(type(minha_idade))
+
+print("Meu primeiro programa em Python")
+Num1 = 100
+Num2 = 350
+print("O resultado de",Num1,"+",Num2,"é:",Num1+Num2)
